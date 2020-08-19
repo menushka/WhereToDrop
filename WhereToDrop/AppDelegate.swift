fda02b5b48a307facba82d5249178fb1f7f94cc1
Menushka Weeratunga <contact@menushka.ca>
2020-08-19 00:56:31 -0400

    Added Main UI

--- WhereToDrop/AppDelegate.swift
+++ WhereToDrop/AppDelegate.swift
@@ -17,12 +17,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = ContentView(dropPath: .constant(""))
 
         // Create the window and set the content view. 
         window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            contentRect: NSRect(x: 0, y: 0, width: 200, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable],
             backing: .buffered, defer: false)
         window.center()
         window.setFrameAutosaveName("Main Window")
--- WhereToDrop/ContentView.swift
+++ WhereToDrop/ContentView.swift
@@ -9,15 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Binding var dropPath: String
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Text("WhereToDrop")
+                .font(Font.system(size: 20, weight: .black, design: .default))
+                .padding(.bottom, 32)
+            HStack(spacing: 0) {
+                TextField("Drop path", text: $dropPath)
+                Button(action: {}, label: {
+                    Text("Select")
+                })
+            }
+            Button(action: {}, label: {
+                Text("Start Redirecting")
+            })
+            .padding(.bottom, 20)
+            Button(action: {}, label: {
+                Text("Stop Redirecting")
+            })
+        }
+        .padding(.all, 12)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(dropPath: .constant("dfsdf"))
+            .previewLayout(PreviewLayout.fixed(width: 200, height: 300))
     }
 }
